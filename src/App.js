796d20ddf07a0f60141eea8229e2dd5c887c9cc6
duynvh <nguyenvohoangduy@gmail.com>
2020-09-08 10:29:23 +0700

    Add i18n

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react'
+import i18n from 'utils/i18n';
 import { Provider } from 'react-redux'
 import 'antd/dist/antd.css'
 import RoutesComponent from 'shared/Routes/RoutesComponent'
@@ -6,11 +7,21 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Spinner from 'components/Common/Spinner'
 import store from 'store/store'
 
-function App() {
+// the hoc
+import { withTranslation } from 'react-i18next';
+
+function App({ t }) {
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
+
   return (
     <Suspense fallback={<Spinner />}>
       <Provider store={store}>
         <Router>
+          <button onClick={() => changeLanguage('vn')}>vn</button>
+          <button onClick={() => changeLanguage('en')}>en</button>
+          <h1>{t('products')}</h1>
           <RoutesComponent />
         </Router>
       </Provider>
@@ -18,4 +29,4 @@ function App() {
   )
 }
 
-export default App
+export default withTranslation()(App);
